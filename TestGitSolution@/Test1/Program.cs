50c4444c25f2f177e90c2b072dc404e3ba5a47b6
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-02-10 11:59:13 +0200

    initial commit of test

---
+++ TestGitSolution@/Test1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Test1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Test
+    {
+        static void Main()
+        {
+        }
+    }
+}
