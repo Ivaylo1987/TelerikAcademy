d48f15bd1d7e979dd23709e63233effb64b8a801
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-02-11 12:55:21 +0200

    Commit Extensions

---
+++ OOP/DotNETExtensions/Demo/Extensions.cs
@@ -0,0 +1,118 @@
+﻿namespace DotNetExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class Extensions
+    {
+        // Extension method Substring on StringBuilder type.
+        public static string SubString(this StringBuilder strBuilder, int index, int length)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (index + length >= strBuilder.Length)
+            {
+                throw new IndexOutOfRangeException("Index out of range! Not enough cahracters in string!");
+            }
+            else
+            {
+                for (int i = index; i < index + length; i++)
+                {
+                    result.Append(strBuilder[i]);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        // Extension method Sum to IEnumerable<T>
+        public static T SumExt<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = 0;
+            foreach (var item in collection)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+        // Extension method Product to IEnumerable<T>
+        public static T Product<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = 1;
+                    
+            foreach (var item in collection)
+            {
+                result *= item;
+            }
+
+            return result;
+        }
+
+        // Extension method Average to IEnumerable<T>
+        public static T AverageExt<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = 0;
+            dynamic count = 0;
+            foreach (var item in collection)
+            {
+                result += item;
+                count++;
+            }
+
+            return result / count;
+        }
+
+        // Extension method Min to IEnumerable<T>
+        public static T MinExt<T>(this IEnumerable<T> collection)
+            where T: IComparable
+        {
+            T minElement = collection.FirstOrDefault();
+
+            if (minElement != null)
+            {
+                foreach (var item in collection)
+                {
+                    if (item.CompareTo(minElement) < 0)
+                    {
+                        minElement = item;
+                    }
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("Null reference occured!");
+            }
+
+            return minElement;
+        }
+
+        // Extension method Мax to IEnumerable<T>
+        public static T MaxExt<T>(this IEnumerable<T> collection)
+            where T : IComparable
+        {
+            T maxElement = collection.FirstOrDefault();
+
+            if (maxElement != null)
+            {
+                foreach (var item in collection)
+                {
+                    if (item.CompareTo(maxElement) > 0)
+                    {
+                        maxElement = item;
+                    }
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("Null reference occured!");
+            }
+
+            return maxElement;
+        }
+    }
+}
---
+++ OOP/DotNETExtensions/Demo/TestExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace DotNetExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TestExtensions
+    {
+        static void Main()
+        {
+            //Testing substring extension method
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("Some str@nge text for testing!");
+            string subStr = builder.SubString(4, 8);
+
+            Console.WriteLine("Substring from StringBuilder:_{0}", subStr);
+
+            // Testing IEnumerable extensions
+            List<int> intEnum = new List<int>();
+
+            for (int i = 1; i < 11; i++)
+            {
+                intEnum.Add(i);
+            }
+
+            // Sum
+            var sumOfEnum = intEnum.SumExt();
+            Console.WriteLine("Sum: {0}", sumOfEnum);
+
+            // Product
+            var productOfEnum = intEnum.Product();
+            Console.WriteLine("Product: {0}", productOfEnum);
+
+            // Average
+            var averageOfEnum = intEnum.AverageExt();
+            Console.WriteLine("Average: {0}", averageOfEnum);
+
+            // Min
+            var minOfEnum = intEnum.MinExt();
+            Console.WriteLine("Min: {0}", minOfEnum);
+
+            // Max
+            var maxOfEnum = intEnum.MaxExt();
+            Console.WriteLine("Max: {0}", maxOfEnum);
+        }
+    }
+}
