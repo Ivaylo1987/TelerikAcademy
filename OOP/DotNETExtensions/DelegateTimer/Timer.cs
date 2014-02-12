3c4c600af5898dd47eb7329e46f47e159d328110
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-02-12 21:43:34 +0200

    timer

---
+++ OOP/DotNETExtensions/DelegateTimer/Timer.cs
@@ -0,0 +1,17 @@
+﻿namespace DelegateTimer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public delegate void ExecuteMethod(string item);
+
+    public class Timer
+    {
+        private Stopwatch timer;
+        public ExecuteMethod MethodToExecute { get; set; }
+    }
+}
