4d4e96be18fa5ac28a13fc2a19e387a869dafe9c
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-02-19 21:07:34 +0200

    cmt

---
+++ OOP/OOPPrinciplesII/Shapes/Demo.cs
@@ -0,0 +1,26 @@
+﻿namespace Shapes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Demo
+    {
+        static void Main()
+        {
+            Shape[] shapes = 
+            {
+                new Rectangle(10, 4),
+                new Triangle(10, 4),
+                new Circle(4)
+            };
+
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine("I am {0} and my surface is: {1}", shape.GetType().Name, shape.CalculateSurface());
+            }
+        }
+    }
+}
