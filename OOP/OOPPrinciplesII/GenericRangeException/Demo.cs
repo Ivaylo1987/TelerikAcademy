a36eabec95c724aacc9343b579c68b762148d62f
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-02-20 00:19:51 +0200

    cmt

---
+++ OOP/OOPPrinciplesII/GenericRangeException/Demo.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericRangeException
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Demo
+    {
+        static void Main()
+        {
+            Console.Write("Please, enter number OUT if the range 1 to 100: ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (number > 100 || number < 1)
+                {
+                    throw new RangeException<int>("Just simulated an error!", 1, 100);
+                }
+            }
+            catch (RangeException<int> exp)
+            {
+
+                Console.WriteLine(exp.Message);
+            }
+            
+        }
+    }
+}
---
+++ OOP/OOPPrinciplesII/GenericRangeException/RangeException.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericRangeException
+{
+    using System;
+    public class RangeException<T> : ApplicationException
+    {
+        private T startOfRange;
+        private T endOfRange;
+
+        public RangeException(string msg, T _startOfRange, T _endOfRange)
+            : this(msg, null, _startOfRange, _endOfRange)
+        {
+        }
+
+        public RangeException(string msg, Exception innerException, T _startOfRange, T _endOfRange)
+            : base(msg, innerException)
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return base.Message + " " + string.Format("Allowed range is: {0} - {1}", this.startOfRange, this.endOfRange);
+            }
+        }
+    }
+}
