b2ae71aec32e9c45d6452368d219a7100fb8940c
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-05-16 21:52:30 +0300

    cmt

--- JS Fundamentals/Strings/Strings/Tasks.js
+++ JS Fundamentals/Strings/Strings/Tasks.js
@@ -26,7 +26,7 @@
     jsConsole.writeLine(areBracketsCorrect('((a+b)/5-d)..as ( 2 + 1(;')); //false
     jsConsole.writeLine(areBracketsCorrect('((a+b)/5-d)..as ( 2 + 1);')); //true
 
-    //task 3
+    //Task 3
     function stringOccurances(input, key) {
         input = input.toLowerCase();
         key - key.toLowerCase();
@@ -43,6 +43,67 @@
     var text = "We are living in an yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
     var key = 'in';
 
-    
     jsConsole.writeLine(stringOccurances(text, key)); //9
+
+    //Task 4
+
+    function changeInAllReagion(input) {
+        input = input.replace(/<upcase>(.+?)<\/upcase>/gi, function (match, p1) { return p1.toUpperCase() });
+        input = input.replace(/<lowcase>(.+?)<\/lowcase>/gi, function (match, p1) { return p1.toLowerCase() });
+        input = input.replace(/<mixcase>(.+?)<\/mixcase>/gi,
+            function (match, p1) {
+                var chanse;
+                var result = '';
+
+                for (var i = 0; i < p1.length; i++) {
+                    chanse = Math.floor(Math.random() * 2);
+
+                    if (chanse) {
+                        result += p1[i].toLowerCase();
+                    }
+                    else {
+                        result += p1[i].toUpperCase();
+                    }
+
+                }
+
+                return result;
+            });
+
+        return input;
+    }
+
+    var txt = 'We are <mixcase>living</mixcase> in a <upcase>yellow submarine</upcase>. We <mixcase>don\'t</mixcase> have <lowcase>anything<\/lowcase> else.';
+    jsConsole.writeLine(changeInAllReagion(txt));
+
+    //Task 5
+    function replaceSpace(input) {
+        return input.replace(/ /g, 'nbsp;');
+    }
+
+    var spaces = 'text    with spaces';
+    jsConsole.writeLine(replaceSpace(spaces));
+
+    //Task 6
+
+    function removeTags(input) {
+        return input.replace(/<[^>]+>/gi, '');
+    }
+
+    var textTags = '<html><head><title>Sample site</title></head><body><div>text<div>more text</div>and more...</div>in body</body></html>';
+    jsConsole.writeLine(removeTags(textTagss));
+
+    // Task 7
+    function jsonURL(input) {
+        var parseResult = input.match(/(.+?):\/\/(.+?)(\/.+)/i);
+
+        return {
+            protocol: parseResult[1],
+            server: parseResult[2],
+            resourse: parseResult[3]
+        }
+    }
+
+    var url = 'http://www.supprot.devbg.org/forum/index.php';
+    jsConsole.writeLine(JSON.stringify(jsonURL(url)));
 })()
