35891e66b915a412ca2d264c7a2a941f110aac61
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-05-30 13:41:12 +0300

    cmt

--- JS UI and DOM/DOM/DOM/selectAllDivs.js
+++ JS UI and DOM/DOM/DOM/selectAllDivs.js
@@ -1,1 +1,29 @@
-﻿
+﻿//---To see result, please open JS console in your preferred browser---//
+
+(function () {
+    // Task 1 with querySelectorAll();
+    function selectDivsQuerySelector() {
+        var divsInDiv = document.querySelectorAll('div > div')
+        return divsInDiv; // returns NodeList
+    }
+
+    console.log(selectDivsQuerySelector()); 
+
+    // Task 1 with getElementsBy();
+    function selectDivsGetElementsByTagName() {
+        var divs = document.getElementsByTagName('div');
+        var result = [];
+
+        for (var i = 0, iLen = divs.length; i < iLen; i++) {
+            for (var j = 0, jLen = divs[i].children.length; j < jLen; j++) {
+                if (divs[i].children[j].nodeName = 'DIV') {
+                    result.push(divs[i].children[j]);
+                }
+            }
+        }
+
+        return result; // returns Array
+    }
+
+    console.log(selectDivsGetElementsByTagName());
+})()
