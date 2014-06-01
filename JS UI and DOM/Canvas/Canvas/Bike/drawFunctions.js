4cf72d06ba9055f928efd52d91ed025ecf85c1b8
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-01 11:26:46 +0300

    cmt

---
+++ JS UI and DOM/Canvas/Canvas/Bike/drawFunctions.js
@@ -0,0 +1,106 @@
+﻿/*global ctx*/
+function getRadian(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function beginDrawCommands() {
+    ctx.beginPath();
+    ctx.save();
+}
+
+function endDrawCommands() {
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+}
+
+function endDrawCommandsNoFill() {
+    ctx.stroke();
+    ctx.restore();
+}
+
+function drawWheels() {
+    var y = 220;
+
+    function drawWheel(x) {
+        beginDrawCommands();
+        ctx.arc(x, y, 60, 0, getRadian(360));
+        endDrawCommands();
+    }
+
+    drawWheel(100);
+    drawWheel(350);
+}
+
+function drawFrame() {
+    var x = 170,
+        y = 150;
+
+    beginDrawCommands();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - 70, y + 70);
+    ctx.lineTo(x + 50, y + 67);
+    ctx.lineTo(x + 150, y);
+    ctx.closePath();
+    endDrawCommandsNoFill();
+}
+
+function drawPedals() {
+    var x = 220,
+        y = 215;
+
+    beginDrawCommands();
+    ctx.arc(x, y, 15, 0, getRadian(360));
+    endDrawCommandsNoFill();
+
+    beginDrawCommands();
+    ctx.moveTo(x - 25, y - 25);
+    ctx.lineTo(x - 10, y - 10);
+    endDrawCommandsNoFill();
+
+    beginDrawCommands();
+    ctx.moveTo(x + 25, y + 25);
+    ctx.lineTo(x + 10, y + 10);
+    endDrawCommandsNoFill();
+
+}
+
+function drawSeat() {
+    var x = 125,
+        y = 118;
+
+    beginDrawCommands();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 50, y);
+    ctx.stroke();
+    ctx.restore();
+
+    beginDrawCommands();
+    ctx.moveTo(x + 25, y);
+    ctx.lineTo(x + 95, y + 100);
+    endDrawCommandsNoFill();
+}
+
+function drawStearing() {
+    var x = 260,
+        y = 118;
+
+    beginDrawCommands();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 50, y - 20);
+    ctx.lineTo(x + 80, y - 60);
+    endDrawCommandsNoFill();
+
+    beginDrawCommands();
+    ctx.moveTo(x + 50, y - 20);
+    ctx.lineTo(x + 90, y + 110);
+    endDrawCommandsNoFill();
+}
+
+function drawBike() {
+    drawWheels();
+    drawFrame();
+    drawPedals();
+    drawSeat();
+    drawStearing();
+}
---
+++ JS UI and DOM/Canvas/Canvas/Person/drawFunctions.js
@@ -0,0 +1,154 @@
+﻿/*global ctx*/
+
+function getRadian(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function beginDrawCommands() {
+    ctx.beginPath();
+    ctx.save();
+}
+
+function endDrawCommands() {
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+}
+
+function drawHat() {
+    var projectX = 130;
+    ctx.fillStyle = "#396693";
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+
+    function drawTop() {
+        var x = 105 + projectX,
+            y = 210;
+
+        beginDrawCommands();
+        ctx.scale(1, 0.4);
+        ctx.arc(x, y, 40, 0, getRadian(360));
+        endDrawCommands();
+    }
+
+    function drawBody() {
+        var x = 65 + projectX,
+            y = 85;
+
+        beginDrawCommands();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + 70);
+        ctx.bezierCurveTo(x + 10, y + 85, x + 70, y + 85, x + 80, y + 70);
+        ctx.lineTo(x + 80, y);
+        endDrawCommands();
+    }
+
+    function drawBottom() {
+        var x = 100 + projectX,
+            y = 800;
+
+        beginDrawCommands();
+        ctx.scale(1, 0.2);
+        ctx.arc(x, y, 80, 0, getRadian(360));
+        endDrawCommands();
+    }
+
+    drawBottom();
+    drawBody();
+    drawTop();
+}
+
+function drawHead() {
+    var projectX = 133;
+    ctx.fillStyle = "#90cad7";
+    ctx.strokeStyle = "#22545f";
+    ctx.lineWidth = 3;
+
+    function drawFace() {
+        var x = 100 + projectX,
+            y = 220;
+
+        beginDrawCommands();
+        ctx.arc(x, y, 70, 0, getRadian(360));
+        endDrawCommands();
+    }
+
+    function drawNose() {
+        var x = 85 + projectX,
+            y = 200;
+
+        beginDrawCommands();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x - 15, y + 30);
+        ctx.lineTo(x, y + 30);
+        endDrawCommands();
+
+    }
+
+    function drawMouth() {
+        var x = 120 + projectX,
+            y = 560;
+
+        beginDrawCommands();
+        ctx.rotate(getRadian(8));
+        ctx.scale(1, 0.4);
+        ctx.arc(x, y, 25, 0, getRadian(360));
+        endDrawCommands();
+    }
+
+    function drawEyes() {
+        var y = 285,
+            yScale = 0.7;
+
+        function drawLeftEye() {
+            var x = 55 + projectX;
+
+            beginDrawCommands();
+            ctx.scale(1, yScale);
+            ctx.arc(x, y, 10, 0, getRadian(360));
+            endDrawCommands();
+        }
+
+        function drawRightEye() {
+            var x = 110 + projectX;
+
+            beginDrawCommands();
+            ctx.scale(1, yScale);
+            ctx.arc(x, y, 10, 0, getRadian(360));
+            endDrawCommands();
+        }
+
+        function drawLeftRetina() {
+            var x = 238 + projectX,
+                y = 199;
+
+            beginDrawCommands();
+            ctx.fillStyle = "#22545f";
+            ctx.scale(0.5, 1);
+            ctx.arc(x, y, 5, 0, getRadian(360));
+            endDrawCommands();
+        }
+
+        function drawRightRetina() {
+            var x = 348 + projectX,
+                y = 199;
+
+            beginDrawCommands();
+            ctx.fillStyle = "#22545f";
+            ctx.scale(0.5, 1);
+            ctx.arc(x, y, 5, 0, getRadian(360));
+            endDrawCommands();
+        }
+
+        drawLeftEye();
+        drawRightEye();
+        drawLeftRetina();
+        drawRightRetina();
+    }
+
+    drawFace();
+    drawEyes();
+    drawNose();
+    drawMouth();
+}
+
