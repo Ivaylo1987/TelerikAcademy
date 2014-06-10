f0eeaad3af9383566b6429abb6254da12ff443bc
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-10 15:39:48 +0300

    cmt

---
+++ JS UI and DOM/DOMManipulation/DOMManipulation/2.CirclingDivs/functions.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var Divs = document.getElementsByClassName("nestedDiv");
+    var angle = 0;
+    var radius = 100;
+    (function move() {
+
+        for (var i = 0; i < 5; i++) {
+            Divs[i].style.left = Math.cos(angle + 2 * Math.PI / Divs.length * i) / radius * 10000 + "px";
+            Divs[i].style.top = Math.sin(angle + 2 * Math.PI / Divs.length * i) / radius * 10000 + "px";
+        }
+
+        angle += 0.1;
+        setTimeout(move, 100);
+    })()
+})()
