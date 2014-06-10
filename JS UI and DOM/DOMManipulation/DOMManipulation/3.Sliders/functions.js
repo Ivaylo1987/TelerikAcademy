3199ca4968f6641e735a53838fd8c826b6f1e8be
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-10 15:28:25 +0300

    cmt

---
+++ JS UI and DOM/DOMManipulation/DOMManipulation/3.Sliders/functions.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    var firstSlide = document.getElementById('firstSlide');
+    var fsecondSlide = document.getElementById('secondSlide');
+    var textArea = document.getElementById('textArea');
+
+    firstSlide.addEventListener('change', function () {
+        textArea.style.color = firstSlide.value;
+    });
+
+    secondSlide.addEventListener('change', function () {
+        textArea.style.backgroundColor = secondSlide.value;
+    });
+})()
