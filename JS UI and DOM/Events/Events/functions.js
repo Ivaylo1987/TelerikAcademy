077530990e91747939ba12ad5a23c1efdf5d86dc
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-10 22:15:01 +0300

    cmt

--- JS UI and DOM/Events/Events/functions.js
+++ JS UI and DOM/Events/Events/functions.js
@@ -1,3 +1,24 @@
 ﻿window.onload = function () {
+    var textArea = document.getElementById('textArea');
+    var select = document.getElementById('select');
+    var addBtn = document.getElementById('add');
+    var removeBtn = document.getElementById('remove');
 
+    addBtn.addEventListener('click', function () {
+        if (textArea.value) {
+            var newOption = document.createElement('option');
+            newOption.innerText = textArea.value;
+            select.appendChild(newOption);
+            textArea.value = '';
+        }
+    });
+
+    removeBtn.addEventListener('click', function () {
+        var options = select.children;
+        for (var i = 0; i < options.length; i++) {
+            if (options[i].selected) {
+                select.removeChild(options[i]);
+            }
+        }
+    });
 }
