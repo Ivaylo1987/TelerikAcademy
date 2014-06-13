b95aad6d7c00badc4b1a8e00bc0ea0f11671f484
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-13 17:09:32 +0300

    cmt

--- JS UI and DOM/jQuery/jQuery/1.Slider/scripts.js
+++ JS UI and DOM/jQuery/jQuery/1.Slider/scripts.js
@@ -1,25 +1,39 @@
 ﻿/// <reference path="E:\Docs\GitRepos\TelerikAcademy\JS UI and DOM\jQuery\jQuery\_reference.js" />
 $(function () {
-    var $container = $('#container');
+    var $container = $('#container'),
+        $nextButton = $('#next'),
+        $prevoiusButton = $('#previous');
+
     $container.children().first().addClass('current');
 
-    var nextButton = $('#next');
-    var prevoiusButton = $('#previous');
-
-    nextButton.on('click', function () {
+    $nextButton.on('click', function () {
         var current = $container.find('.current');
+        current.removeClass('current');
 
         if (current.next().length === 0) {
-            current.removeClass('current');
             $container.children().first().addClass('current');
         }
         else {
-            current.removeClass('current').next().addClass('current');
+            current.next().addClass('current');
         }
-        
     })
 
-    prevoiusButton.on('click', function () {
-        $container.find('.current').removeClass('current').prev().addClass('current');
+    $prevoiusButton.on('click', function () {
+        var current = $container.find('.current');
+        current.removeClass('current');
+
+        if (current.prev().length === 0) {
+            $container.children().last().addClass('current');
+        }
+        else {
+            current.prev().addClass('current');
+        }
     })
+
+    // slef sliding ever 4 seconds
+    function moveimage() {
+        $nextButton.click();
+        setTimeout(moveimage, 4000);
+    }
+    moveimage();
 })
---
+++ JS UI and DOM/jQuery/jQuery/2.AddBeforeAdnAfter/scripts.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="E:\Docs\GitRepos\TelerikAcademy\JS UI and DOM\jQuery\jQuery\_reference.js" />
+$(function () {
+    $('div').prepend('<p>' + 'I am added before!' + '</p>');
+    $('div').append('<p>' + 'I am added after!' + '</p>');
+})
