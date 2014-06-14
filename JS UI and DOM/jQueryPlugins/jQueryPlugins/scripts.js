9a739a713da43c547b0831a3dcc067547ecbd2d1
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-14 18:31:46 +0300

    cmt

--- JS UI and DOM/jQueryPlugins/jQueryPlugins/scripts.js
+++ JS UI and DOM/jQueryPlugins/jQueryPlugins/scripts.js
@@ -2,12 +2,36 @@
 $(function () {
 
     $.fn.dropDownList = function () {
-        var selector = this.selector;
-        var wrapper = $('<div />').addClass(selector + '-list-container');
+        var classOfgenerated = this.selector.substr(1, this.selector.length  - 1) + '-list-container';
+        var wrapper = $('<div />').addClass(classOfgenerated);
+        var ul = $('<ul />').addClass(classOfgenerated);
+        var li;
+
+        this.children().each(function () {
+            li = $('<li />');
+            if (this.selected) {
+                li.addClass('selected');
+            }
+
+            li.html(this.innerHTML);
+            li.addClass(classOfgenerated);
+            li.attr('data-values', this.value);
+            ul.append(li);
+        });
 
         this.hide();
-        $('body').append(wrapper)
+        $('body').append(wrapper.append(ul))
+        var clickBlick = $('.' + classOfgenerated);
+
+        // Makes list selectable
+        clickBlick.on('click','li', function () {
+            $clicked = $(this);
+            $clicked.parent().find('.selected').removeClass('selected');
+            $clicked.addClass('selected');
+        })
     }
 
-    $('#dropdown').dropDownList();
+    $('#generateList').on('click', function () {
+        $('#dropdown').dropDownList();
+    })
 })
