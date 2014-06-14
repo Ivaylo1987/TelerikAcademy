a552e15c6530ac02d5276877fa1d86c81d23c4a8
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-14 16:47:59 +0300

    cmt

---
+++ JS UI and DOM/jQueryPlugins/jQueryPlugins/scripts.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="E:\Docs\GitRepos\TelerikAcademy\JS UI and DOM\jQueryPlugins\jQueryPlugins\libs/_reference.js" />
+$(function () {
+
+    $.fn.dropDownList = function () {
+        var selector = this.selector;
+        var wrapper = $('<div />').addClass(selector + '-list-container');
+
+        this.hide();
+        $('body').append(wrapper)
+    }
+
+    $('#dropdown').dropDownList();
+})
