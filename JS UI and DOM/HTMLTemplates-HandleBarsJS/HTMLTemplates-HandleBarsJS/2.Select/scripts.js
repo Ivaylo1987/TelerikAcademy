943d65f18ccb5a6ce7fc1464ef6951b4897b1408
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-06-15 12:11:30 +0300

    cmt

---
+++ JS UI and DOM/HTMLTemplates-HandleBarsJS/HTMLTemplates-HandleBarsJS/2.Select/scripts.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="E:\Docs\GitRepos\TelerikAcademy\JS UI and DOM\HTMLTemplates-HandleBarsJS\HTMLTemplates-HandleBarsJS\libs/_reference.js" />
+
+var items = [{
+    value: 1,
+    text: 'One'
+}, {
+    value: 2,
+    text: 'Two'
+}, {
+    value: 3,
+    text: 'Three'
+}, {
+    value: 4,
+    text: 'Four'
+}, {
+    value: 5,
+    text: 'five'
+}, {
+    value: 6,
+    text: 'Six'
+}];
+
+var teplateHTML = document.getElementById('template').innerHTML;
+var templateCompiled = Handlebars.compile(teplateHTML);
+var target = document.getElementById('root');
+
+target.innerHTML += templateCompiled({
+    options: items
+});
