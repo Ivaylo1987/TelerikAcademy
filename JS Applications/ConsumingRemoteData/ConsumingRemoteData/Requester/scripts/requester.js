7d21b75fbf5f1488543e5927882d1ee792bfa5d3
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-07-25 16:20:46 +0300

    cmt

---
+++ JS Applications/ConsumingRemoteData/ConsumingRemoteData/Requester/scripts/requester.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="E:\Docs\GitRepos\TelerikAcademy\JS Applications\ConsumingRemoteData\ConsumingRemoteData\Scripts/_references.js" />
+var requester = (function () {
+    'use strict'
+
+    var getJSON = function (url, headers) {
+        return $.ajax({
+            type: 'GET',
+            url: url,
+            headers: headers
+        });
+    }
+
+    var postJSON = function (url, data, headers) {
+        return $.ajax({
+            type: 'POST',
+            url: url,
+            data: data,
+            headers: headers
+        })
+    }
+
+    return {
+        getJSON: getJSON,
+        postJSON: postJSON
+    }
+}())
---
+++ JS Applications/ConsumingRemoteData/ConsumingRemoteData/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-2.1.1.js" />
+/// <reference path="require.js" />
+/// <reference path="../requester/scripts/requester.js" />
