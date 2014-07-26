d9c7b4a0573acc5a60b712466197965cea139009
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-07-26 13:55:37 +0300

    cmt

--- JS Applications/ConsumingRemoteData/ConsumingRemoteData/Scripts/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="jquery-2.1.1.js" />
-/// <reference path="require.js" />
-/// <reference path="../tasks 1 and 3 - requester/scripts/requester.js" />
