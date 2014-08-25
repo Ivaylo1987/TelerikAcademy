e28da7410b32ca27412da762dc08aaa2400f3d5e
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-25 16:29:50 +0300

    cmt

---
+++ DSA/LinearDataStructures/FindMajorant/Demo.cs
@@ -0,0 +1,18 @@
+﻿namespace FindMajorant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Demo
+    {
+        static void Main()
+        {
+            var sequence = new List<int>() { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
+
+            var majorant = sequence.First(el => sequence.Count(i => i == el) >= (sequence.Count / 2 + 1));
+
+            Console.WriteLine("Majorant is: {0}", majorant);
+        }
+    }
+}
---
+++ DSA/LinearDataStructures/FirstFiftyMembers/Demo.cs
@@ -0,0 +1,43 @@
+﻿namespace FirstFiftyMembers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Demo
+    {
+        private static void PrintFirstFiftyMembers(int seed)
+        {
+            var numberQueue = new Queue<int>();
+            var result = new List<int>();
+            var count = 0;
+
+            numberQueue.Enqueue(seed);
+            while(true)
+            {
+                if (count == 50)
+                {
+                    break;
+                }
+
+                var current = numberQueue.Dequeue();
+                numberQueue.Enqueue(current + 1);
+                numberQueue.Enqueue(2 * current + 1);
+                numberQueue.Enqueue(current + 2);
+
+                result.Add(current);
+                count++;
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        static void Main()
+        {
+            Console.Write("Please, enter seed for the sequence: ");
+            var input = int.Parse(Console.ReadLine());
+
+            PrintFirstFiftyMembers(input);
+        }
+    }
+}
