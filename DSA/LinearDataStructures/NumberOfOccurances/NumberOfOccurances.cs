538548cb75039e537e034a6237ac17fab8057f2b
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-25 15:05:08 +0300

    cmt

---
+++ DSA/LinearDataStructures/NumberOfOccurances/NumberOfOccurances.cs
@@ -0,0 +1,35 @@
+﻿namespace NumberOfOccurances
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class NumberOfOccurances
+    {
+        static void Main()
+        {
+            List<int> sequence = new List<int>() { 2, 2, 1, 1, 3, 3, 3, 3, 3, 4, 4, 4, 4, 1, 1, 1, 1, 1 };
+            Console.WriteLine("Before:");
+            Console.WriteLine(string.Join(", ", sequence));
+
+            var numbersAndOccurances = new Dictionary<int, int>();
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (numbersAndOccurances.ContainsKey(sequence[i]))
+                {
+                    numbersAndOccurances[sequence[i]]++;
+                }
+                else
+                {
+                    numbersAndOccurances.Add(sequence[i], 1);
+                }
+            }
+
+            foreach (var number in numbersAndOccurances)
+            {
+                Console.WriteLine("{0} -> {1} times", number.Key, number.Value);
+            }
+        }
+    }
+}
---
+++ DSA/LinearDataStructures/RemoveOdd/RemoveOdd.cs
@@ -0,0 +1,42 @@
+﻿namespace RemoveOdd
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class RemoveOdd
+    {
+        static void Main()
+        {
+            List<int> sequence = new List<int>() { 2, 2, 1, 1, 3, 3, 3, 3, 3, 4, 4, 4, 4, 1, 1, 1, 1, 1 };
+            Console.WriteLine("Before:");
+            Console.WriteLine(string.Join(", ", sequence));
+
+            var numbersAndOccurances = new Dictionary<int, int>();
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (numbersAndOccurances.ContainsKey(sequence[i]))
+                {
+                    numbersAndOccurances[sequence[i]]++;
+                }
+                else
+                {
+                    numbersAndOccurances.Add(sequence[i], 1);
+                }
+            }
+
+            foreach (var number in numbersAndOccurances)
+            {
+                if (number.Value % 2 != 0)
+                {
+                    sequence.RemoveAll(el => el == number.Key);
+                }
+            }
+
+            Console.WriteLine("After:");
+            sequence.RemoveAll(e => e < 0);
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+    }
+}
