16d2d36aef1b806cec240848139c9cc5af0cd45b
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-27 00:19:22 +0300

    cmt

---
+++ DSA/TreesAndTreeTraversal/TreeManupulations/Demo.cs
@@ -0,0 +1,63 @@
+﻿namespace TreeManupulations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Demo
+    {
+        private static int FindRoot(IDictionary<int, Node<int>> nodes)
+        {
+            var result = nodes.First(pair => !pair.Value.HasParent);
+            return result.Key;
+        }
+
+        private static IEnumerable<int> Findleaves(IDictionary<int, Node<int>> nodes)
+        {
+            var result = nodes.Where(pair => pair.Value.Children.Count == 0);
+            return result.Select(pair => pair.Key);
+        }
+
+        private static IEnumerable<int> FindMiddleChildren(IDictionary<int, Node<int>> nodes)
+        {
+            var result = nodes.Where(pair => pair.Value.Children.Count != 0 && pair.Value.HasParent);
+            return result.Select(pair => pair.Key);
+        }
+
+        static void Main()
+        {
+            var numberOfNodes = int.Parse(Console.ReadLine());
+            var nodes = new Dictionary<int, Node<int>>();
+
+            for (int i = 0; i < numberOfNodes - 1; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+
+                var parent = int.Parse(input[0]);
+                var child = int.Parse(input[1]);
+
+                if (!nodes.ContainsKey(child))
+                {
+                    nodes[child] = new Node<int>(child);
+                }
+
+                nodes[child].HasParent = true;
+
+                if (!nodes.ContainsKey(parent))
+                {
+                    nodes[parent] = new Node<int>(parent);
+                }
+
+                nodes[parent].Children.Add(nodes[child]);
+
+            }
+
+            // Find root
+            Console.WriteLine(FindRoot(nodes));
+            // Find Leaves
+            Console.WriteLine(string.Join(", ", Findleaves(nodes)));
+            // Find Middle Children
+            Console.WriteLine(string.Join(", ", FindMiddleChildren(nodes)));
+        }
+    }
+}
---
+++ DSA/TreesAndTreeTraversal/TreeManupulations/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace TreeManupulations
+{
+    using System;
+    using System.Collections.Generic;
+    class Node<T>
+    {
+        public Node()
+        {
+            this.Children = new List<Node<T>>();
+        }
+
+        public Node(T value)
+            :this()
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public List<Node<T>> Children { get; set; }
+
+        public bool  HasParent { get; set; }
+    }
+}
