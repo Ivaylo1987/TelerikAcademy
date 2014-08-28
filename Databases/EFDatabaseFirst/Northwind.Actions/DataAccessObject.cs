c38fa7a607911984d7f0240652f38b6509413ef6
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-28 13:22:25 +0300

    cmt

---
+++ Databases/EFDatabaseFirst/Northwind.Actions/DataAccessObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Northwind.Actions
+{
+    using System.Data.Entity;
+    public class DataAccessObject
+    {
+        private DbContext dbContext;
+
+        public DataAccessObject(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
---
+++ Databases/EFDatabaseFirst/Northwind.Actions/Demo.cs
@@ -0,0 +1,15 @@
+﻿namespace Northwind.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Northwind.Data;
+
+    class Demo
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
