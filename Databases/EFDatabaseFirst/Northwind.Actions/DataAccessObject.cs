141eec42c7184f2deb74e33f22cc548c018c6c04
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-28 23:48:43 +0300

    cmt

--- Databases/EFDatabaseFirst/Northwind.Actions/DataAccessObject.cs
+++ Databases/EFDatabaseFirst/Northwind.Actions/DataAccessObject.cs
@@ -5,6 +5,9 @@
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
+    using System.Data.Entity.Core.Objects;
+    using System.Data.Entity.Infrastructure;
+    using System.Data.SqlClient;
 
     public class DataAccessObject
     {
@@ -18,12 +21,12 @@
             }
         }
 
-        public static void UpdateCustomers(Customer customer, string id)
+        public static void UpdateCustomers(string customerName, string id)
         {
             using (var northwindDb = new NorthwindEntities())
             {
                 var toUpdate = northwindDb.Customers.First(c => c.CustomerID == id);
-                toUpdate = customer;
+                toUpdate.ContactName = customerName;
                 northwindDb.SaveChanges();
             }
         }
@@ -69,16 +72,76 @@
             {
                 var sales = northWindDb.Orders
                                         .Where(o => o.ShipRegion == region && start < o.OrderDate && o.OrderDate < end)
-                                        .Select(o => new { o.ShipName, o.ShippedDate}).ToList();
+                                        .Select(o => new { o.ShipName, o.ShippedDate }).ToList();
 
                 return sales;
             }
         }
 
-
         public static void CreateTwin()
         {
-            
+            IObjectContextAdapter northWindDb = new NorthwindEntities();
+            string northwindScript = northWindDb.ObjectContext.CreateDatabaseScript();
+            string createNorthwindCloneDB = "USE master; " +
+                                            "CREATE DATABASE NorthwindTwin; ";
+
+            SqlConnection dbCon = new SqlConnection("Server=.\\SQLEXPRESS; " +
+                                                    "Database=master; Integrated Security=true");
+            dbCon.Open();
+            using (dbCon)
+            {
+                SqlCommand createCloneDB = new SqlCommand(createNorthwindCloneDB, dbCon);
+                createCloneDB.ExecuteNonQuery();
+
+                string changeDB = "USE  NorthwindTwin; ";
+                SqlCommand changeDataB = new SqlCommand(changeDB, dbCon);
+                changeDataB.ExecuteNonQuery();
+
+                SqlCommand cloneDB = new SqlCommand(northwindScript, dbCon);
+                cloneDB.ExecuteNonQuery();
+            }
+        }
+
+        public static void ConsurentChange()
+        {
+            using (var northWindDbOther = new NorthwindEntities())
+            {
+                var customerOne = northWindDbOther.Customers.First(c => c.CustomerID == "Pesho");
+
+                northWindDbOther.Customers.Remove(customerOne);
+
+                UpdateCustomers("Gosho", "Pesho");
+
+                northWindDbOther.SaveChanges();
+
+            }
+        }
+
+        public static void InsertOrders(params Order[] orders)
+        {
+            using (var northWindDb = new NorthwindEntities())
+            {
+                using (var dbTransaction = northWindDb.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (var item in orders)
+                        {
+                            northWindDb.Orders.Add(item);
+                        }
+
+                        northWindDb.SaveChanges();
+                        dbTransaction.Commit();
+                        Console.WriteLine("Successfully Inserted");
+                    }
+                    catch (Exception ex)
+                    {
+                        dbTransaction.Rollback();
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+            }
         }
     }
 }
--- Databases/EFDatabaseFirst/Northwind.Actions/Demo.cs
+++ Databases/EFDatabaseFirst/Northwind.Actions/Demo.cs
@@ -12,7 +12,7 @@
             // 2.Create a DAO class with static methods which provide functionality for inserting, modifying and deleting customers.
             // Write a testing class.
             var peshoCustomer = new Customer { CustomerID = "Pesho", CompanyName = "ICompany" };
-            //DataAccessObject.InsertCustomer(peshoCustomer);
+            DataAccessObject.InsertCustomer(peshoCustomer);
 
             // 3.Write a method that finds all customers who have orders made in 1997 and shipped to Canada.
             DataAccessObject.CustomersWithOrdersIn1997InCanada();
@@ -28,14 +28,39 @@
             // 5.Write a method that finds all the sales by specified region and period (start / end dates).
 
             var region = "RJ";
-            var startDate = new DateTime( 1996, 1, 1);
-            var endtDate = new DateTime( 1997, 10, 10);
+            var startDate = new DateTime(1996, 1, 1);
+            var endtDate = new DateTime(1997, 10, 10);
             var sales = DataAccessObject.AllSalesByPeriod(region, startDate, endtDate);
 
             foreach (var sale in sales)
             {
                 Console.WriteLine(sale);
             }
+
+            // 06.Create a database called NorthwindTwin with the same structure as Northwind using the features from DbContext.
+            // Find for the API for schema generation in MSDN or in Google.
+
+            // Uncoment this to create the NorthWindTwin table
+            DataAccessObject.CreateTwin();
+
+            // 07.Try to open two different data contexts and perform concurrent changes on the same records. What will happen at SaveChanges()? How to deal with it?
+
+            DataAccessObject.ConsurentChange();
+
+            // NOT Applicable any more in EF 6
+            // 08.By inheriting the Employee entity class create a class which allows employees to access their corresponding
+            // territories as property of type EntitySet<T>.
+
+
+            // 09.Create a method that places a new order in the Northwind database. The order should contain several order items.
+            // Use transaction to ensure the data consistency.
+
+            var correntOrder = new Order { ShipName = "Correct" };
+            var inCorrentOrder = new Order { ShipName = "InCorrect", ShipPostalCode = "12345678901" };
+
+            DataAccessObject.InsertOrders(correntOrder, inCorrentOrder);
+
+            //Sorry no time for 10 and 11 :(
         }
     }
 }
