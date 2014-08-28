22bf3059a98dd3eae08fdf9df2913f1eeadacf47
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-28 18:08:02 +0300

    cmt

--- Databases/EFDatabaseFirst/Northwind.Actions/DataAccessObject.cs
+++ Databases/EFDatabaseFirst/Northwind.Actions/DataAccessObject.cs
@@ -1,13 +1,84 @@
 ﻿namespace Northwind.Actions
 {
+    using System;
+    using Northwind.Data;
+    using System.Collections.Generic;
     using System.Data.Entity;
+    using System.Linq;
+
     public class DataAccessObject
     {
-        private DbContext dbContext;
-
-        public DataAccessObject(DbContext dbContext)
+        // 2.Create a DAO class with static methods which provide functionality for inserting, modifying and deleting customers.
+        public static void InsertCustomer(Customer customer)
         {
-            this.dbContext = dbContext;
+            using (var northwindDb = new NorthwindEntities())
+            {
+                northwindDb.Customers.Add(customer);
+                northwindDb.SaveChanges();
+            }
+        }
+
+        public static void UpdateCustomers(Customer customer, string id)
+        {
+            using (var northwindDb = new NorthwindEntities())
+            {
+                var toUpdate = northwindDb.Customers.First(c => c.CustomerID == id);
+                toUpdate = customer;
+                northwindDb.SaveChanges();
+            }
+        }
+
+        public static void DeleteCustomers(string id)
+        {
+            using (var northwindDb = new NorthwindEntities())
+            {
+                var toDelete = northwindDb.Customers.First(c => c.CustomerID == id);
+                northwindDb.Customers.Remove(toDelete);
+                northwindDb.SaveChanges();
+            }
+        }
+
+        public static void CustomersWithOrdersIn1997InCanada()
+        {
+            using (var northWind = new NorthwindEntities())
+            {
+                var customers = northWind.Orders
+                    .Where(o => o.OrderDate.Value.Year == 1997 && o.ShipCountry == "Canada")
+                    .Select(o => new { o.Customer.CustomerID, o.Customer.ContactName });
+                //Console.WriteLine(customers);
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine("Id: {0} Name: {1}", customer.CustomerID, customer.ContactName);
+                }
+            }
+        }
+
+        public static IEnumerable<object> CustomersWithOrdersIn1997InCanadaWithQuery()
+        {
+            using (var northWind = new NorthwindEntities())
+            {
+                var query = "SELECT c.[ContactName] FROM  Orders AS o LEFT JOIN [dbo].[Customers] AS c ON o.[CustomerID] = c.[CustomerID] WHERE (1997 = (DATEPART (year, o.[OrderDate]))) AND (N'Canada' = o.[ShipCountry])";
+
+                return northWind.Database.SqlQuery<string>(query).ToList();
+            }
+        }
+
+        public static IEnumerable<object> AllSalesByPeriod(string region, DateTime start, DateTime end)
+        {
+            using (var northWindDb = new NorthwindEntities())
+            {
+                var sales = northWindDb.Orders
+                                        .Where(o => o.ShipRegion == region && start < o.OrderDate && o.OrderDate < end)
+                                        .Select(o => new { o.ShipName, o.ShippedDate}).ToList();
+
+                return sales;
+            }
+        }
+
+
+        public static void CreateTwin()
+        {
+            
         }
     }
 }
--- Databases/EFDatabaseFirst/Northwind.Actions/Demo.cs
+++ Databases/EFDatabaseFirst/Northwind.Actions/Demo.cs
@@ -9,7 +9,33 @@
     {
         static void Main()
         {
+            // 2.Create a DAO class with static methods which provide functionality for inserting, modifying and deleting customers.
+            // Write a testing class.
+            var peshoCustomer = new Customer { CustomerID = "Pesho", CompanyName = "ICompany" };
+            //DataAccessObject.InsertCustomer(peshoCustomer);
 
+            // 3.Write a method that finds all customers who have orders made in 1997 and shipped to Canada.
+            DataAccessObject.CustomersWithOrdersIn1997InCanada();
+
+            // 4.Implement previous by using native SQL query and executing it through the DbContext.
+            var customers = DataAccessObject.CustomersWithOrdersIn1997InCanadaWithQuery();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+
+            // 5.Write a method that finds all the sales by specified region and period (start / end dates).
+
+            var region = "RJ";
+            var startDate = new DateTime( 1996, 1, 1);
+            var endtDate = new DateTime( 1997, 10, 10);
+            var sales = DataAccessObject.AllSalesByPeriod(region, startDate, endtDate);
+
+            foreach (var sale in sales)
+            {
+                Console.WriteLine(sale);
+            }
         }
     }
 }
