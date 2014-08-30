f61eca4b9a9d8c73b1d8d6e423cc8627029ad883
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-30 12:43:06 +0300

    cmt

---
+++ Databases/MicraDocGettingStarted/Chart/GenerateChart.cs
@@ -0,0 +1,145 @@
+﻿namespace Chart
+{
+    using MigraDoc.DocumentObjectModel;
+    using MigraDoc.Rendering;
+    using MigraDoc.DocumentObjectModel.Shapes.Charts;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    class GenerateChart
+    {
+        /// <summary>
+        /// Defines the styles used in the pdf document.
+        /// </summary>
+        public static void DefineStyles(Document document)
+        {
+            // Get the predefined style Normal.
+            Style style = document.Styles["Normal"];
+            // Because all styles are derived from Normal, the next line changes the 
+            // font of the whole document. Or, more exactly, it changes the font of
+            // all styles and paragraphs that do not redefine the font.
+            style.Font.Name = "Times New Roman";
+
+            // Heading1 to Heading9 are predefined styles with an outline level. An outline level
+            // other than OutlineLevel.BodyText automatically creates the outline (or bookmarks) 
+            // in PDF.
+
+            style = document.Styles["Heading1"];
+            style.Font.Name = "Tahoma";
+            style.Font.Size = 14;
+            style.Font.Bold = true;
+            style.Font.Color = Colors.DarkBlue;
+            style.ParagraphFormat.PageBreakBefore = true;
+            style.ParagraphFormat.SpaceAfter = 6;
+
+            style = document.Styles["Heading2"];
+            style.Font.Size = 12;
+            style.Font.Bold = true;
+            style.ParagraphFormat.PageBreakBefore = false;
+            style.ParagraphFormat.SpaceBefore = 6;
+            style.ParagraphFormat.SpaceAfter = 6;
+
+            style = document.Styles["Heading3"];
+            style.Font.Size = 10;
+            style.Font.Bold = true;
+            style.Font.Italic = true;
+            style.ParagraphFormat.SpaceBefore = 6;
+            style.ParagraphFormat.SpaceAfter = 3;
+
+            style = document.Styles[StyleNames.Header];
+            style.ParagraphFormat.AddTabStop("16cm", TabAlignment.Right);
+
+            style = document.Styles[StyleNames.Footer];
+            style.ParagraphFormat.AddTabStop("8cm", TabAlignment.Center);
+
+            // Create a new style called TextBox based on style Normal
+            style = document.Styles.AddStyle("TextBox", "Normal");
+            style.ParagraphFormat.Alignment = ParagraphAlignment.Justify;
+            style.ParagraphFormat.Borders.Width = 2.5;
+            style.ParagraphFormat.Borders.Distance = "3pt";
+            style.ParagraphFormat.Shading.Color = Colors.SkyBlue;
+
+            // Create a new style called TOC based on style Normal
+            style = document.Styles.AddStyle("TOC", "Normal");
+            style.ParagraphFormat.AddTabStop("16cm", TabAlignment.Right, TabLeader.Dots);
+            style.ParagraphFormat.Font.Color = Colors.Blue;
+        }
+
+        public static void DefineChart(Document document)
+        {
+            Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph("Projects", "Heading1");
+
+            paragraph.AddBookmark("Chart");
+
+            document.LastSection.AddParagraph("DeadLines By Month", "Heading2");
+
+            Chart chart = new Chart();
+            chart.Left = 0;
+
+            chart.Width = Unit.FromCentimeter(16);
+            chart.Height = Unit.FromCentimeter(12);
+
+            Series series = chart.SeriesCollection.AddSeries();
+            series.ChartType = ChartType.Column2D;
+            series.Add(new double[] { 1, 17, 45, 5, 3, 20, 11, 23, 8, 19 });
+            series.HasDataLabel = true;
+
+            series = chart.SeriesCollection.AddSeries();
+            series.ChartType = ChartType.Line;
+            series.Add(new double[] { 41, 7, 5, 45, 13, 10, 21, 13, 18, 9 });
+
+            XSeries xseries = chart.XValues.AddXSeries();
+            xseries.Add("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+            chart.XAxis.MajorTickMark = TickMarkType.Outside;
+            chart.XAxis.Title.Caption = "X-Axis";
+
+            chart.YAxis.MajorTickMark = TickMarkType.Outside;
+            chart.YAxis.HasMajorGridlines = true;
+
+            chart.PlotArea.LineFormat.Color = Colors.DarkGray;
+            chart.PlotArea.LineFormat.Width = 1;
+
+            document.LastSection.Add(chart);
+        }
+
+        public static Document CreateDocument()
+        {
+            // Create a new MigraDoc document
+            Document document = new Document();
+            document.Info.Title = "Hello, MigraDoc";
+            document.Info.Subject = "Demonstrates an excerpt of the capabilities of MigraDoc.";
+            document.Info.Author = "Stefan Lange";
+
+            DefineStyles(document);
+
+            DefineChart(document);
+
+            return document;
+        }
+
+        static void Main()
+        {
+            // Create a MigraDoc document
+            Document document = CreateDocument();
+
+            //string ddl = MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToString(document);
+            //MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToFile(document, "MigraDoc.mdddl");
+
+            PdfDocumentRenderer renderer = new PdfDocumentRenderer(true, PdfSharp.Pdf.PdfFontEmbedding.Always);
+            renderer.Document = document;
+
+            renderer.RenderDocument();
+
+            // Save the document...
+            string filename = "MigraDocChart6.pdf";
+            renderer.PdfDocument.Save(filename);
+
+            // ...and start a viewer.
+            Process.Start(filename);
+        }
+    }
+}
---
+++ Databases/MicraDocGettingStarted/GettingStarted/Demo.cs
@@ -0,0 +1,110 @@
+﻿namespace GettingStarted
+{
+    using MigraDoc.DocumentObjectModel;
+    using MigraDoc.DocumentObjectModel.Tables;
+    using MigraDoc.Rendering;
+    using PdfSharp.Pdf;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Demo
+    {
+        static void Main()
+        {
+            //var document = new Document();
+
+            //Section section = document.AddSection();
+            //section.AddParagraph("Hello, World!");
+            //section.AddParagraph();
+
+            //Paragraph paragraph = section.AddParagraph();
+            //paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
+            //paragraph.AddFormattedText("Hello, World!", TextFormat.Underline);
+
+            //FormattedText ft = paragraph.AddFormattedText("Small text", TextFormat.Bold);
+            //ft.Font.Size = 6;
+
+            //PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer(false, PdfFontEmbedding.Always);
+            //pdfRenderer.Document = document;
+
+            //pdfRenderer.RenderDocument();
+
+            //string filename = "HelloWorld.pdf";
+            //pdfRenderer.PdfDocument.Save(filename);
+
+            //Process.Start(filename);
+
+            Document document = new Document();
+            document.Info.Author = "Rolf Baxter";
+            document.Info.Keywords = "MigraDoc, Examples, C#";
+
+            // Get the A4 page size
+            Unit width, height;
+            PageSetup.GetPageSize(PageFormat.A4, out width, out height);
+
+            // Add a section to the document and configure it such that it will be in the centre
+            // of the page
+            Section section = document.AddSection();
+            section.PageSetup.PageHeight = height;
+            section.PageSetup.PageWidth = width;
+            section.PageSetup.LeftMargin = 0;
+            section.PageSetup.RightMargin = 0;
+            section.PageSetup.TopMargin = height / 2;
+
+            // Create a table so that we can draw the horizontal lines
+            Table table = new Table();
+            table.Borders.Width = 1; // Default to show borders 1 pixel wide Column
+            var column = table.AddColumn(width);
+            column.Format.Alignment = ParagraphAlignment.Center;
+
+            double fontHeight = 36;
+            Font font = new Font("Times New Roman", fontHeight);
+
+            // Add a row with a single cell for the first line
+            Row row = table.AddRow();
+            Cell cell = row.Cells[0];
+
+            cell.Format.Font.Color = Colors.Black;
+            cell.Format.Font.ApplyFont(font);
+            cell.Borders.Left.Visible = false;
+            cell.Borders.Right.Visible = false;
+            cell.Borders.Bottom.Visible = false;
+
+            cell.AddParagraph("Hello, World!");
+
+            // Add a row with a single cell for the second line
+            row = table.AddRow();
+            cell = row.Cells[0];
+
+            cell.Format.Font.Color = Colors.Black;
+            cell.Format.Alignment = ParagraphAlignment.Left;
+            cell.Format.Font.ApplyFont(font);
+            cell.Borders.Left.Visible = false;
+            cell.Borders.Right.Visible = false;
+            cell.Borders.Top.Visible = false;
+
+            cell.AddParagraph("This is some long text that *will* auto-wrap when the edge of the page is reached");
+
+            document.LastSection.Add(table);
+
+            // Create a renderer
+            PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer();
+
+            // Associate the MigraDoc document with a renderer
+            pdfRenderer.Document = document;
+
+            // Layout and render document to PDF
+            pdfRenderer.RenderDocument();
+
+            // Save and show the document
+            pdfRenderer.PdfDocument.Save
+
+            ("TestDocument.pdf");
+            Process.Start("TestDocument.pdf");
+        }
+    }
+}
