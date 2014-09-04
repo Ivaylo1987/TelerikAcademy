ff06c5ba02a861f8d68642d4558d22a78aeeb5b7
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-09-05 02:40:58 +0300

    cmt

---
+++ Databases/MongoDbDemos/Mongo.DbConnector/Connector.cs
@@ -0,0 +1,16 @@
+﻿namespace Mongo.DbConnector
+{
+    using MongoDB.Driver;
+
+    public class Connector
+    {
+        public  MongoDatabase GetDatabase (string connectionString, string databaseName)
+        {
+            var db = new MongoClient(connectionString);
+            var server = db.GetServer();
+            var database = server.GetDatabase(databaseName);
+
+            return database;
+        }
+    }
+}
---
+++ Databases/Transactions/ATM.Client/Demo.cs
@@ -0,0 +1,66 @@
+﻿namespace ATM.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ATM.Model;
+    using System.Data;
+
+    class Demo
+    {
+        private static bool CheckIfCardIsValid(string cardNumber, string pin)
+        {
+            var result = false;
+
+            using (var db = new ATMEntities())
+            {
+                if (db.CardAccounts.Any(ca => ca.CardNumber == cardNumber && ca.CardPIN == pin))
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        private static bool CheckIfAccounBallanceIsSufficient(string cardNumber, decimal transferSum)
+        {
+            var result = false;
+
+            using (var db = new ATMEntities())
+            {
+                var account = db.CardAccounts.First(ca => ca.CardNumber == cardNumber);
+
+                if (account.CardCash >= transferSum)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            using (var db = new ATMEntities())
+            {
+                var tran = db.Database.BeginTransaction(IsolationLevel.RepeatableRead);
+
+                var cardNumber = Console.ReadLine();
+                var pin = Console.ReadLine();
+
+                if (!CheckIfCardIsValid(cardNumber, pin))
+                {
+                    tran.Rollback();
+                }
+
+                if (!CheckIfAccounBallanceIsSufficient(cardNumber, 200))
+                {
+                    tran.Rollback();
+                }
+            }
+        }
+    }
+}
