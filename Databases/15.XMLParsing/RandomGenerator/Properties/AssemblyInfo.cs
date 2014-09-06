393c003d757a78270c9b889cce77b7681ade3487
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-09-06 11:29:17 +0300

    cmt

--- Databases/15.XMLParsing/RandomGenerator/Properties/AssemblyInfo.cs
+++ Databases/15.XMLParsing/RandomGenerator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mongo.DbConnector")]
+[assembly: AssemblyTitle("RandomGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mongo.DbConnector")]
+[assembly: AssemblyProduct("RandomGenerator")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c4515138-2544-4895-b542-210d1e0a3a16")]
+[assembly: Guid("2b693296-dcbf-41f5-9584-3b9e9eab76c9")]
 
 // Version information for an assembly consists of the following four values:
 //
