7be639073f4df1a4fae43e7265eaf339aa2e77df
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-10-17 18:53:50 +0300

    cmt

---
+++ WebAPI/ExamPrep/Application.WCF/AlertsService.svc.cs
@@ -0,0 +1,34 @@
+﻿using Application.Data.Repositories;
+using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Application.WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AlertsService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AlertsService.svc or AlertsService.svc.cs at the Solution Explorer and start debugging.
+    public class AlertsService : IAlertsService
+    {
+        private IApplicationData data;
+
+
+        public AlertsService()
+            :this(new ApplicationData())
+        {
+        }
+
+        public AlertsService(IApplicationData data)
+        {
+            this.data = data;
+        }
+
+        public ICollection<Alert> GetAlerts()
+        {
+            return this.data.Alerts.All().ToArray();
+        }
+    }
+}
---
+++ WebAPI/ExamPrep/Application.WCF/IAlertsService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Application.WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAlertsService" in both code and config file together.
+    [ServiceContract]
+    public interface IAlertsService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "api/alerts")]
+        ICollection<Alert> GetAlerts();
+    }
+}
